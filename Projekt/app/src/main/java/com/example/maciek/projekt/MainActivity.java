3b5df791970ec16c7d5016f37a17b8536b01ea71
RedBeetle777 <mfaron5@gmail.com>
2018-05-09 20:23:59 +0200

    added Listener

--- Projekt/app/src/main/java/com/example/maciek/projekt/MainActivity.java
+++ Projekt/app/src/main/java/com/example/maciek/projekt/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.maciek.projekt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -19,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         list = (ListView) findViewById(R.id.list_view);
+        final EditText et = findViewById(R.id.base);
 
         String cars[] = {"Mercedes", "Fiat", "Ferrari", "Aston Martin", "Lamborghini", "Skoda", "Volkswagen", "Audi", "Citroen"};
 
@@ -26,7 +30,21 @@ public class MainActivity extends AppCompatActivity {
         carL.addAll( Arrays.asList(cars) );
 
         adapter = new ArrayAdapter<String>(this, R.layout.row, carL);
-
         list.setAdapter(adapter);
+
+        Button add = findViewById(R.id.add);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = et.getText().toString();
+                adapter.add(text);
+
+            }
+        };
+
+        add.setOnClickListener(listener);
+
+
+
     }
 }
