0715980d9fe00ef37fbbd73637fe352f2c9ec799
RedBeetle777 <mfaron5@gmail.com>
2018-05-09 19:30:09 +0200

    added list_view

--- Projekt/app/src/main/java/com/example/maciek/projekt/MainActivity.java
+++ Projekt/app/src/main/java/com/example/maciek/projekt/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.maciek.projekt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView list ;
+    private ArrayAdapter<String> adapter ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = (ListView) findViewById(R.id.list_view);
+
+        String cars[] = {"Mercedes", "Fiat", "Ferrari", "Aston Martin", "Lamborghini", "Skoda", "Volkswagen", "Audi", "Citroen"};
+
+        ArrayList<String> carL = new ArrayList<String>();
+        carL.addAll( Arrays.asList(cars) );
+
+        adapter = new ArrayAdapter<String>(this, R.layout.row, carL);
+
+        list.setAdapter(adapter);
     }
 }
